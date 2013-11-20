e0adb40967b731853456edb918f6e2f32cabd400
Xabier Maeztu <trisketonni@gmail.com>
2013-11-20 12:24:08 +0100

    More layouts

---
+++ themes/coworker/layouts/book/book-layout.tpl.php
@@ -0,0 +1,19 @@
+<div class="container">
+  <div class="row-fluid">
+    <div class="span12">
+      <div class="span4">
+      <div class="row-fluid">
+        
+          <?php print $content['cover'] ?>
+        </div>
+      </div>
+       <div class="span8">
+      <div class="row-fluid">
+        <div class="span12">
+          <?php print $content['title'] ?>
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
