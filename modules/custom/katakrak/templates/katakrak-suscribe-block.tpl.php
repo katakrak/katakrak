1ba50ef35b22863e33a584a17174bdc597e1e807
Xabier Maeztu <trisketonni@gmail.com>
2013-12-03 13:39:16 +0100

    Suscribe block

---
+++ modules/custom/katakrak/templates/katakrak-suscribe-block.tpl.php
@@ -0,0 +1,4 @@
+<div class="katakrak-suscribe-form-help">
+  <?php print t('Quieres estar al tanto de todo lo que ocurren en Katakrak? Recibe nuestro boletin') ?>
+</div>
+<?php print render($form) ?>
