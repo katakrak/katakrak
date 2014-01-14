fcbbbc0953ff7195d1c45a9b0b01c7427dae8bc0
Xabier Maeztu <trisketonni@gmail.com>
2014-01-14 20:07:46 +0100

    GA module and changes to block

--- modules/custom/katakrak/templates/katakrak-suscribe-block.tpl.php
+++ modules/custom/katakrak/templates/katakrak-suscribe-block.tpl.php
@@ -1,4 +1,5 @@
 <div class="katakrak-suscribe-form-help">
-  <?php print t('Quieres estar al tanto de todo lo que ocurren en Katakrak? Recibe nuestro boletin') ?>
+  <?php print t('En breve tendremos la web lista. Mientras tanto puedes dejar tu mail para recibir todas las noticias en cuanto tengamos la web funcionando.') ?><br />
+  <?php print t('Laister izango dugu prest webgunea. Momentuz zure emaila utzi ahala dezakezu gure berriak jasotzeko prest dagoenean.') ?>
 </div>
 <?php print render($form) ?>
