05cbb30d70447d003a109426ec32e86b4bc848ca
Xabier Maeztu <trisketonni@gmail.com>
2014-01-28 11:13:10 +0100

    Subscribe block

--- modules/custom/katakrak/templates/katakrak-suscribe-block.tpl.php
+++ modules/custom/katakrak/templates/katakrak-suscribe-block.tpl.php
@@ -1,5 +1,5 @@
 <div class="katakrak-suscribe-form-help">
-  <?php print t('En breve tendremos la web lista. Mientras tanto puedes dejar tu mail para recibir todas las noticias en cuanto tengamos la web funcionando.') ?><br />
-  <?php print t('Laister izango dugu prest webgunea. Momentuz zure emaila utzi ahala dezakezu gure berriak jasotzeko prest dagoenean.') ?>
+  <?php print 'En breve tendremos la web lista. Mientras tanto puedes dejar tu mail para recibir todas las noticias en cuanto tengamos la web funcionando.' ?><br />
+  <?php print 'Laster izango dugu prest webgunea. Momentuz zure e-posta utz dezakezu. Buletina prest dugunean hasiko zara jasotzen.' ?>
 </div>
 <?php print render($form) ?>
