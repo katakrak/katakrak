5ed785a5c4cb5f18d6070bb0bd1d4ed0c3d3100e
Xabier Maeztu <trisketonni@gmail.com>
2014-02-24 11:59:18 +0100

    Social block

--- modules/custom/katakrak/templates/katakrak-social-block.tpl.php
+++ modules/custom/katakrak/templates/katakrak-social-block.tpl.php
@@ -1,5 +1,5 @@
 <ul>
-  <li class="ts-facebook"><a href="https://www.facebook.com/pages/La-Hormiga-At%C3%B3mica/118927254784460?fref=ts" target="_blank"><span class="ts-icon"></span><span class="ts-text">Facebook</span></a></li>
+  <li class="ts-facebook"><a href="https://www.facebook.com/katakrak54" target="_blank"><span class="ts-icon"></span><span class="ts-text">Facebook</span></a></li>
   <li class="ts-twitter"><a href="http://twitter.com/katakrak54" target="_blank"><span class="ts-icon"></span><span class="ts-text">Twitter</span></a></li>
   <li class="ts-rss"><a href="<?php print url('events/feed') ?>"><span class="ts-icon"></span><span class="ts-text">RSS</span></a></li>
   <li class="ts-phone"><a href="tel:+34948225520"><span class="ts-icon"></span><span class="ts-text">948225520</span></a></li>
