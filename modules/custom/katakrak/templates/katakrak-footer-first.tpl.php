4e7bc9fe39313c72ae2c0224f0f0e94ba9719572
Xabier Maeztu <trisketonni@gmail.com>
2014-03-17 20:39:07 +0100

    Removing map

--- modules/custom/katakrak/templates/katakrak-footer-first.tpl.php
+++ modules/custom/katakrak/templates/katakrak-footer-first.tpl.php
@@ -1,7 +1,7 @@
 <p><?php print t('Libros, cafés y algo más. Debates y pensamiento. Un espacio para el común.')?></p>
 <div class="footer-about-us">
 <ul style="font-size: 13px;">
-  <li class="icon-map-marker">C/Mayor 54 K Nagusia <br>Pamplona-Iruñea 31001</li>
+  <li class="">C/Mayor 54 K Nagusia <br>Pamplona-Iruñea 31001</li>
 <li class="icon-phone">948225520</li>
 <li class="icon-envelope-alt"><a href="mailto:info@katakrak.net">info@katakrak.net</a></li>
 </ul></div>
