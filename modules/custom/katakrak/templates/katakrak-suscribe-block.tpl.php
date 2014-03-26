37826fa8391fa703c7273cf0d71fecddd33d683d
Xabier Maeztu <trisketonni@gmail.com>
2014-03-26 11:20:14 +0100

    Adding envelope

--- modules/custom/katakrak/templates/katakrak-suscribe-block.tpl.php
+++ modules/custom/katakrak/templates/katakrak-suscribe-block.tpl.php
@@ -1,6 +1,13 @@
   <div class="row-fluid">
     <div class="suscribe-block suscribe-block-help span4">
-      <?php print t('¿Quieres estar al tanto de todo lo que ocurre en Katakrak? Suscribete al <span class="colored-text">boletín de noticias</span>') ?>
+      <div class="row-fluid">
+        <div class="span4">
+        <i class="fa fa-envelope fa-5x"></i>
+        </div>
+      <div class="span8">
+        <?php print t('¿Quieres estar al tanto de todo lo que ocurre en Katakrak? Suscribete al <span class="colored-text">boletín de noticias</span>') ?>
+      </div>
+      </div>
     </div>
     <div class="suscribe-block span6">
       <?php print drupal_render($form['mail']) ?>
