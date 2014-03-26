55461928c088975048a17e49d36e1be5fd00acda
Xabier Maeztu <trisketonni@gmail.com>
2014-03-26 13:20:03 +0100

    Adding horarios block

---
+++ modules/custom/katakrak/templates/katakrak-horarios.tpl.php
@@ -0,0 +1,8 @@
+<?php print t("Abierto de lunes a sábado*") ?><br>
+<h6><?php print t("Librería") ?></h6>
+<?php print t("9:30-14:00 - 16:30-21:00") ?>
+<h6><?php print t("Cantina") ?></h6>
+LMM de 08:00-22:00 - JVS 08:00-01:00
+<br>
+<br>
+* El lunes de 14:00 a 16:30 cerrado por asamblea
