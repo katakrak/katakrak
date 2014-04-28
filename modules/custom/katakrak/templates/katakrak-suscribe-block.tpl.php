c6d6497d88afce647a09233b7d2d830c2cbabf7b
Xabier Maeztu <trisketonni@gmail.com>
2014-04-28 10:30:46 +0200

    Adding  blue envelope

--- modules/custom/katakrak/templates/katakrak-suscribe-block.tpl.php
+++ modules/custom/katakrak/templates/katakrak-suscribe-block.tpl.php
@@ -2,7 +2,7 @@
     <div class="suscribe-block suscribe-block-help span4">
       <div class="row-fluid">
         <div class="span4">
-        <i class="fa fa-envelope fa-5x"></i>
+        <i class="fa fa-envelope fa-5x blue-envelope"></i>
         </div>
       <div class="span8">
         <?php print t('¿Quieres estar al tanto de todo lo que ocurre en Katakrak? Suscribete al <span class="colored-text">boletín de noticias</span>') ?>
