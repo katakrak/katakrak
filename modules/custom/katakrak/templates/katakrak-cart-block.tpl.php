6b8b6e6bc5bc2858802be3e51d1bdd420e7b10d8
Xabier Maeztu <trisketonni@gmail.com>
2014-05-20 11:03:11 +0200

    Sharethis options + Cart

--- modules/custom/katakrak/templates/katakrak-cart-block.tpl.php
+++ modules/custom/katakrak/templates/katakrak-cart-block.tpl.php
@@ -1,9 +1,7 @@
-<div class="<?php print $class ?>">
 <a href="<?php print url('cart') ?>">
-  <i class="fa fa-shopping-cart fa-3x"></i>
+  <i class="fa fa-shopping-cart fa-3x <?php print $class ?>"></i>
    <span>
     (<?php print $num_items ?>)
   </span>
 
 </a>
-</div>
