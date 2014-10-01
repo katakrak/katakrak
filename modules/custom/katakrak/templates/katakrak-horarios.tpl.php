0895eccdf11887edb75f570538c45a7c1836d7c3
Xabier Maeztu <trisketonni@gmail.com>
2014-10-01 10:33:09 +0200

    Adding several changes

--- modules/custom/katakrak/templates/katakrak-horarios.tpl.php
+++ modules/custom/katakrak/templates/katakrak-horarios.tpl.php
@@ -1,6 +1,6 @@
 <?php print t("Abierto de lunes a sábado*") ?><br>
 <h6><?php print t("Librería") ?></h6>
-<?php print t("9:30-14:00 - 16:30-21:00") ?>
+<?php print t("10:0-14:00 - 17:00-21:00") ?>
 <h6><?php print t("Cantina") ?></h6>
 <?php print t("LMM de 08:00-22:00 - JVS 08:00-01:00") ?>
 <br>
