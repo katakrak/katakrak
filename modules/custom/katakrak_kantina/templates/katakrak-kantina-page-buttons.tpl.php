b98f555eac679b4e021aa787f60976e52923b897
Xabier Maeztu <trisketonni@gmail.com>
2015-03-12 18:36:42 +0100

    MOre changes

--- modules/custom/katakrak_kantina/templates/katakrak-kantina-page-buttons.tpl.php
+++ modules/custom/katakrak_kantina/templates/katakrak-kantina-page-buttons.tpl.php
@@ -11,13 +11,13 @@
 <div class="row hidden-md hidden-lg">
   
     <div class="col-xs-4">
-      <?php print l('<i class="fa fa-5x fa-cutlery"></i><h3>'.t("Menú del día").'</h3>', $menu_dia_path, array('html' => TRUE)); ?>
+      <?php print l('<i class="fa fa-9x fa-cutlery"></i><h3>'.t("Menú del día").'</h3>', $menu_dia_path, array('html' => TRUE)); ?>
     </div>
     <div class="col-xs-4">
-      a
+      <?php print l(theme('image', array('path' => drupal_get_path('theme', 'coworker').'/images/burger.png')).'<h3>Fines de semana</h3>', $menu_dia_path, array('html' => TRUE)) ?>
     </div>
     <div class="col-xs-4">
-      a
+      
     </div>
   
 </div>
