c4cbcb1ef72a7836d7c097e9d9c8bc05229b3f4b
Xabier Maeztu <trisketonni@gmail.com>
2015-03-12 15:03:01 +0100

    Adding more blocks to kantina

--- modules/custom/katakrak_kantina/templates/katakrak-kantina-page-buttons.tpl.php
+++ modules/custom/katakrak_kantina/templates/katakrak-kantina-page-buttons.tpl.php
@@ -1,17 +1,22 @@
-<div class="hidden-phone hidden-tablet">
-  <i class="fa fa-10x fa-cutlery"></i>
+<div class="hidden-xs hidden-sm">
   
+  <div class="kantina-menu-dia-block">
+    <?php print l('<i class="fa fa-10x fa-cutlery"></i><h3>'.t("Menú del día").'</h3>', $menu_dia_path, array('html' => TRUE)); ?>
+  </div>
+  <div class="kantina-fin-semana-block">
+    <?php print l(theme('image', array('path' => drupal_get_path('theme', 'coworker').'/images/burger.png')).'<h3>Fines de semana</h3>', $menu_dia_path, array('html' => TRUE)) ?>
+  </div>
 </div>
 
-<div class="hidden-desktop">
+<div class="row hidden-md hidden-lg">
   
-    <div class="col_one_third">
+    <div class="col-xs-4">
+      <?php print l('<i class="fa fa-5x fa-cutlery"></i><h3>'.t("Menú del día").'</h3>', $menu_dia_path, array('html' => TRUE)); ?>
+    </div>
+    <div class="col-xs-4">
       a
     </div>
-    <div class="col_one_third">
-      a
-    </div>
-    <div class="col_one_third col_last">
+    <div class="col-xs-4">
       a
     </div>
   
