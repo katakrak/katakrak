9d1781f9447f7204b09190504ff075cc9ad56304
Xabier Maeztu <trisketonni@gmail.com>
2015-03-12 13:07:57 +0100

    Updating kantina page

---
+++ modules/custom/katakrak_kantina/templates/katakrak-kantina-page-buttons.tpl.php
@@ -0,0 +1,19 @@
+<div class="hidden-phone hidden-tablet">
+  <i class="fa fa-10x fa-cutlery"></i>
+  
+</div>
+
+<div class="hidden-desktop">
+  
+    <div class="col_one_third">
+      a
+    </div>
+    <div class="col_one_third">
+      a
+    </div>
+    <div class="col_one_third col_last">
+      a
+    </div>
+  
+</div>
+
