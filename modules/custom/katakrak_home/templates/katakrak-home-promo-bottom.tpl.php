90278567888600ba9db0f01902b2df6d95dd529a
Xabier Maeztu <trisketonni@gmail.com>
2015-04-14 17:01:57 +0200

    Updating katakrak_home

--- modules/custom/katakrak_home/templates/katakrak-home-promo-bottom.tpl.php
+++ modules/custom/katakrak_home/templates/katakrak-home-promo-bottom.tpl.php
@@ -1,8 +1,12 @@
 <div class="container">
   <div class="row">
-    <div class="col-lg-3">Kantina</div>
-    <div class="col-lg-3">Liburuak</div>
-    <div class="col-lg-3">Ardotegia</div>
-    <div class="col-lg-3">Potemkin</div>
+    <div class="col-lg-3 col-xs-6">
+      <div class="kantina_logo">
+        <?php print l(theme('image', array('path' => drupal_get_path('theme', 'coworker'). '/images/kantina/kantina.png')), 'cantina', array('html' => TRUE)) ?>
+      </div>
+    </div>
+    <div class="col-lg-3 col-xs-6">Librería </div>
+    <div class="col-lg-3 col-xs-6">Gela nagusia</div>
+    <div class="col-lg-3 col-xs-6">Vinoteca</div>
   </div>
 </div>
--- modules/custom/katakrak_home/templates/katakrak-home-promo-right.tpl.php
+++ modules/custom/katakrak_home/templates/katakrak-home-promo-right.tpl.php
@@ -1,11 +1,21 @@
 <div class="row">
   <div class="col-xs-4 col-lg-12 col-md-12 col-sm-12">
-    <?php print $link_agenda ?><h3>Katakrak Agenda</h3>
+    <?php print $link_agenda_icon ?>
+    <div class="promo-right-text">
+      <h3><?php print l(t('Katakrak Agenda'), 'agenda') ?></h3>
+      <?php print t('Todos nuestros eventos de un vistazo') ?>
+    </div>
   </div>
   <div class="col-xs-4 col-lg-12 col-md-12 col-sm-12 promo-right">
-    <?php print $link_boletin ?><h3>Harpidetu buletinera</h3>
+    <?php print $link_boletin_icon ?>
+    <div class="promo-right-text">
+      <h3><?php print l(t('Boletin semanal'), 'boletin') ?></h3>
+    </div>
   </div>
   <div class="col-xs-4 col-lg-12 col-md-12 col-sm-12 promo-right">
-    <?php print $link_nagusia ?><h3>Gela nagusia</h3>
+    <?php print $link_blog_icon ?>
+    <div class="promo-right-text">
+      <h3><?php print l(t('Blog'), 'blog') ?></h3>
+    </div>
   </div>
 </div>
