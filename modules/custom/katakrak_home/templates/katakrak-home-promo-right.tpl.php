6a850b9c2e0a28f0aaf5cdb03e163a144ab49292
Xabier Maeztu <trisketonni@gmail.com>
2015-04-20 14:52:07 +0200

    Styles

--- modules/custom/katakrak_home/templates/katakrak-home-promo-right.tpl.php
+++ modules/custom/katakrak_home/templates/katakrak-home-promo-right.tpl.php
@@ -1,5 +1,5 @@
 <div class="row">
-  <div class="col-xs-4 col-lg-12 col-md-12 col-sm-12 promo-right">
+  <div class="col-xs-4 col-lg-12 col-md-12 col-sm-12 promo-right-first">
     <?php print $link_agenda_icon ?>
     <div class="promo-right-text">
       <h3><?php print l(t('Katakrak Agenda'), 'agenda') ?></h3>
@@ -9,13 +9,15 @@
   <div class="col-xs-4 col-lg-12 col-md-12 col-sm-12 promo-right">
     <?php print $link_tienda_icon ?>
     <div class="promo-right-text">
-      <h3><?php print l(t('Tienda'), 'tienda') ?></h3>
+      <h3><?php print l(t('Tienda online'), 'tienda') ?></h3>
+      <?php print t('Recibe tus libros en casa') ?>
     </div>
   </div>
   <div class="col-xs-4 col-lg-12 col-md-12 col-sm-12 promo-right">
     <?php print $link_blog_icon ?>
     <div class="promo-right-text">
       <h3><?php print l(t('Blog'), 'blog') ?></h3>
+      <?php print t('Reseñas, críticas y demás') ?>
     </div>
   </div>
 </div>
