27325f8344f1c5660ddb1294194dc2ae9e9aa910
Xabier Maeztu <trisketonni@gmail.com>
2015-05-25 15:53:11 +0200

    refinando buscador

--- modules/custom/libro/templates/libreria-home-search.tpl.php
+++ modules/custom/libro/templates/libreria-home-search.tpl.php
@@ -1,9 +1,13 @@
-<h1><?php print t('@count libros a tu alcance', array('@count' => $total_count))?></h1>
+
 <?php if ($terms): ?>
+<h1><?php print t('@count libros a tu alcance', array('@count' => $total_count))?></h1>
   <p><?php print t('En ')?>
-    <?php foreach ($terms as $term): ?>
-      <?php print l($term->name, 'taxonomy/term/'.$term->tid) ?> 
+    <?php foreach ($terms as $i => $term): ?>
+      <?php print l($term->name, 'taxonomy/term/'.$term->tid) ?><?php print ($i < count($terms) - 1) ? ',': ''; ?>
     <?php endforeach; ?>
+    <?php print t('y otras tantas') ?>
   </p>
+<?php else: ?>
+  <h1><?php print t('@count libros en !term', array('@count' => $total_count, '!term' => $seccion->name))?></h1>
 <?php endif; ?>
 <?php print drupal_render($form) ?>
