6c110b854f4ad9c61964ba47f894ea2ff0e06dc7
Xabier Maeztu <trisketonni@gmail.com>
2015-09-03 10:19:14 +0200

    Ordutegiak aldatzen

--- modules/custom/katakrak/templates/katakrak-horarios.tpl.php
+++ modules/custom/katakrak/templates/katakrak-horarios.tpl.php
@@ -1,7 +1,7 @@
 <?php print t("Abierto de lunes a sábado*") ?><br>
 <h6><?php print t("Librería") ?></h6>
-<?php print t("10:00-14:00 - 17:00-21:00") ?>
+<?php print t("10:00-21:00") ?>
 <h6><?php print t("Cantina") ?></h6>
-<?php print t("LMM de 08:30-22:00 - JVS 08:30-01:00") ?>
+<?php print t("LMM de 10:00-23:00 - JVS 10:00-01:00") ?>
 <br>
 <br>
