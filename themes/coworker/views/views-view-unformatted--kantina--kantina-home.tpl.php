ed93a37a24be12ab0c6eea3bd61c1ce7b82c2603
Xabier Maeztu <trisketonni@gmail.com>
2015-09-10 15:14:35 +0200

    Fixing kantina page

--- themes/coworker/views/views-view-unformatted--kantina--kantina-home.tpl.php
+++ themes/coworker/views/views-view-unformatted--kantina--kantina-home.tpl.php
@@ -10,47 +10,23 @@
  */
 ?>
 <div class="row">
-  <div class="col-lg-7">
-    <div class="home-event">
-      <?php print l(
-      theme('image_style', array('style_name' => 'home_agenda_main', 'path' => $view->result[0]->field_field_imagen[0]['raw']['uri'])).
-        '<div class="home-events-title home-events-title-main">
-        <h3>'.$view->result[0]->node_title.'</h3>'.
-        $view->result[0]->field_field_event_date[0]['rendered']['#markup'].
-        '</div>',
-      'node/'.$view->result[0]->nid, array('html' => TRUE)); ?>
+  <div class="col-lg-7 col-md-7">
+    <div class="">
+      <?php print theme('image_style', array('style_name' => 'kantina_large', 'path' => $view->result[0]->field_field_imagen[0]['raw']['uri'])) ?>
+    </div>
+  </div>
+  <div class="col-lg-5 col-md-5">
+    <?php print theme('image_style', array('style_name' => 'kantina_medium', 'path' => $view->result[1]->field_field_imagen[0]['raw']['uri'])) ?>
+  </div> 
+</div>
 
-    </div>
-    <div class="home-event">
-      <?php print l(
-      theme('image_style', array('style_name' => 'home_agenda_main_secondary', 'path' => $view->result[3]->field_field_imagen[0]['raw']['uri'])).
-        '<div class="home-events-title">
-        <h3>'.$view->result[3]->node_title.'</h3>'.
-        $view->result[1]->field_field_event_descripcion[0]['rendered']['#markup'].
-        $view->result[3]->field_field_event_date[0]['rendered']['#markup'].'
-      </div>',
-      'node/'.$view->result[3]->nid, array('html' => TRUE, 'attributes' => array('class' => array('home_events_second'))));  ?>
-      
+<div class="row kantina-images-second-row">
+  <div class="col-lg-5 col-md-5">
+    <div class="">
+      <?php print theme('image_style', array('style_name' => 'kantina_medium', 'path' => $view->result[2]->field_field_imagen[0]['raw']['uri'])) ?>
     </div>
   </div>
-  <div class="col-lg-5">
-    <div class="home-event">
-      <?php print l(
-        theme('image_style', array('style_name' => 'home_agenda', 'path' => $view->result[1]->field_field_imagen[0]['raw']['uri'])).
-        '<div class="home-events-title home-events-title-second">
-          <h3>'.$view->result[1]->node_title.'</h3>'.
-        $view->result[1]->field_field_event_descripcion[0]['rendered']['#markup'].
-        $view->result[1]->field_field_event_date[0]['rendered']['#markup'].'
-        </div>',
-        'node/'.$view->result[1]->nid, array('html' => TRUE));  ?>
-    </div>
-    <div class="home-event">
-    <?php print l(
-      theme('image_style', array('style_name' => 'home_agenda', 'path' => $view->result[2]->field_field_image[0]['raw']['uri'])).
-      '<div class="home-events-title">
-        <h3>'.$view->result[2]->node_title.'</h3>'.$view->result[2]->field_field_event_date[0]['rendered']['#markup'].'
-      </div>',
-      'node/'.$view->result[2]->nid, array('html' => TRUE, 'attributes' => array('class' => array('home_events_second'))));  ?>
-    </div>
-  </div>
+  <div class="col-lg-7 col-md-7">
+    <?php print theme('image_style', array('style_name' => 'kantina_large', 'path' => $view->result[3]->field_field_imagen[0]['raw']['uri'])) ?>
+  </div> 
 </div>
