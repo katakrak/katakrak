33cdea91af7cf0fdb17817dde93646a33ca6be70
Xabier Maeztu <trisketonni@gmail.com>
2015-09-21 15:25:00 +0200

    Adding date in nice format

--- modules/custom/katakrak_agenda/templates/agenda-date.tpl.php
+++ modules/custom/katakrak_agenda/templates/agenda-date.tpl.php
@@ -1,6 +1,5 @@
 <div class="entry_date">
   <div class="day"><?php print format_date($time, 'custom', 'd'); ?></div>
-  <div class="month"><?php print format_date($time, 'custom', 'M'); ?></div>
-  <div class="weekday"><?php print format_date($time, 'custom', 'D'); ?></div>
+  <div class="month"><?php print format_date($time, 'custom', 'F'); ?></div>
   <div class="hour"><?php print format_date($time, 'custom', 'G:i'); ?></div>
 </div>
--- themes/coworker/views/views-view-unformatted--agenda--home.tpl.php
+++ themes/coworker/views/views-view-unformatted--agenda--home.tpl.php
@@ -7,17 +7,17 @@
  * @ingroup views_templates
  * 
         '<p>'.$view->result[0]->field_field_event_descripcion[0]['raw']['summary'].'</p>'.
- */dpm($view)
+ */
 ?>
 <div class="row">
-  <div class="col-lg-7">
+  <div class="col-lg-7 col-md-7">
     <div class="home-event">
       <?php print l(
       theme('image_style', array('style_name' => 'home_agenda_main', 'path' => $view->result[0]->field_field_event_image[0]['raw']['uri'])).
-        '<div class="home-events-title home-events-title-main">
-        <h3>'.$view->result[0]->node_title.'</h3>'.
-        $view->result[0]->field_field_event_type[0]['rendered']['#markup'].
-        $view->result[0]->field_field_event_date[0]['rendered']['#markup'].
+        '<div class="home-events-title home-events-title-main">'.
+        theme('agenda_date', array('time' => $view->result[0]->field_field_event_date[0]['raw']['value'])).
+        '<div class="event_info"><h2>'.$view->result[0]->node_title.'</h2>'.
+        '<h3>'.$view->result[0]->field_field_event_type[0]['rendered']['#markup'].'</h3></div>'.
         '</div>',
       'node/'.$view->result[0]->nid, array('html' => TRUE)); ?>
 
@@ -25,34 +25,34 @@
     <div class="home-event">
       <?php print l(
       theme('image_style', array('style_name' => 'home_agenda_main_secondary', 'path' => $view->result[3]->field_field_event_image[0]['raw']['uri'])).
-        '<div class="home-events-title">
-        <h3>'.$view->result[3]->node_title.'</h3>'.
-        $view->result[3]->field_field_event_type[0]['rendered']['#markup'].
-        $view->result[3]->field_field_event_date[0]['rendered']['#markup'].'
-      </div>',
+        '<div class="home-events-title home-events-title-main">'.
+        theme('agenda_date', array('time' => $view->result[3]->field_field_event_date[0]['raw']['value'])).
+        '<div class="event_info"><h2>'.$view->result[3]->node_title.'</h2>'.
+        '<h3>'.$view->result[3]->field_field_event_type[0]['rendered']['#markup'].'</h3></div>'.
+        '</div>',
       'node/'.$view->result[3]->nid, array('html' => TRUE, 'attributes' => array('class' => array('home_events_second'))));  ?>
       
     </div>
   </div>
-  <div class="col-lg-5">
+  <div class="col-lg-5 col-md-5">
     <div class="home-event">
       <?php print l(
         theme('image_style', array('style_name' => 'home_agenda', 'path' => $view->result[1]->field_field_event_image[0]['raw']['uri'])).
-        '<div class="home-events-title home-events-title-second">
-          <h3>'.$view->result[1]->node_title.'</h3>'.
-        '<div>'.$view->result[1]->field_field_event_type[0]['rendered']['#markup'].'</div>'.
-        $view->result[1]->field_field_event_date[0]['rendered']['#markup'].'
-        </div>',
+        '<div class="home-events-title home-events-title-second">'.
+        theme('agenda_date', array('time' => $view->result[1]->field_field_event_date[0]['raw']['value'])).
+        '<div class="event_info"><h2>'.$view->result[1]->node_title.'</h2>'.
+        '<h3>'.$view->result[1]->field_field_event_type[0]['rendered']['#markup'].'</h3></div>'.
+        '</div>',
         'node/'.$view->result[1]->nid, array('html' => TRUE));  ?>
     </div>
     <div class="home-event">
     <?php print l(
       theme('image_style', array('style_name' => 'home_agenda', 'path' => $view->result[2]->field_field_event_image[0]['raw']['uri'])).
-      '<div class="home-events-title">
-        <h3>'.$view->result[2]->node_title.'</h3>'.
-      $view->result[2]->field_field_event_type[0]['rendered']['#markup'].
-      $view->result[2]->field_field_event_date[0]['rendered']['#markup'].'
-      </div>',
+        '<div class="home-events-title home-events-title-second">'.
+        theme('agenda_date', array('time' => $view->result[2]->field_field_event_date[0]['raw']['value'])).''.
+        '<div class="event_info"><h2>'.$view->result[2]->node_title.'</h2>'.
+        '<h3>'.$view->result[2]->field_field_event_type[0]['rendered']['#markup'].'</h3></div>'.
+        '</div>',
       'node/'.$view->result[2]->nid, array('html' => TRUE, 'attributes' => array('class' => array('home_events_second'))));  ?>
     </div>
   </div>
