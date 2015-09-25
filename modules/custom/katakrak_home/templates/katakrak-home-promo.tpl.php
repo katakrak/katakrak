5d65fec813acf376ee5679118d9c31141f400b4d
Xabier Maeztu <trisketonni@gmail.com>
2015-09-25 16:16:55 +0200

    Multiple changes

--- modules/custom/katakrak_home/templates/katakrak-home-promo.tpl.php
+++ modules/custom/katakrak_home/templates/katakrak-home-promo.tpl.php
@@ -1,19 +1,30 @@
   <div class="container">
     <div class="row">
-      <div class="col-lg-3 col-xs-6">
-        <?php print $link_kantina_icon ?>
-        <h3>Cantina</h3>
+      <div class="col col-md-4 col-sm-6">
+        <div class="views-field-field-image">
+          <a href="<?php print url('cantina')?>"></a>
+          <?php print theme('image_style', array('path' => 'public://katakrak_home/kantina.jpg', 'style_name' => 'home_promo')) ?>
+          <div class="portfolio_shader"></div>
+          <div class="name"><h3><?php print t('Cantina') ?></h3></div>
+        </div>
       </div>
-      <div class="col-lg-3 col-xs-6">
-        <?php print $link_libreria_icon ?>
-        <h3>Librería</h3>
+      <div class="col col-md-4 col-sm-6">
+        <div class="views-field-field-image">
+          <a href="<?php print url('libreria')?>"></a>
+          
+          <?php print theme('image_style', array('path' => 'public://katakrak_home/libreria_2.jpg', 'style_name' => 'home_promo')) ?>
+          <div class="portfolio_shader"></div>
+          <div class="name"><h3><?php print t('Librería') ?></h3></div>
+        </div>
+       
       </div>
-      <div class="col-lg-3 col-xs-6">
-        <h3>Sala mayor</h3>
-      </div>
-      <div class="col-lg-3 col-xs-6">
-        <?php print $link_vinoteca_icon ?>
-        <h3>Vinoteca</h3>
+      <div class="col col-md-4 col-sm-6">
+        <div class="views-field-field-image">
+          <a href="<?php print url('libreria')?>"></a>
+          <?php print theme('image_style', array('path' => 'public://katakrak_home/nagusia.jpg', 'style_name' => 'home_promo')) ?>
+          <div class="portfolio_shader"></div>
+          <div class="name"><h3><?php print t('Sala nagusia') ?></h3></div>
+        </div>
       </div>
     </div>
   </div>
