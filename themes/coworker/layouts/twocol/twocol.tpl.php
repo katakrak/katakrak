5bc49ecb0f52d4fc7a09e7cc441b8a501268827e
Xabier Maeztu <trisketonni@gmail.com>
2015-12-09 17:50:04 +0100

    New variant

--- themes/coworker/layouts/twocol/twocol.tpl.php
+++ themes/coworker/layouts/twocol/twocol.tpl.php
@@ -1,9 +1,9 @@
 <div class="container">
   <div class="row">
-    <div class="col-lg-3">
+    <div class="col-md-9">
       <?php print $content['left'] ?>
      </div>
-    <div class="col-lg-9">
+    <div class="col-md-3">
       <?php print $content['right'] ?>
     </div>
   </div>
