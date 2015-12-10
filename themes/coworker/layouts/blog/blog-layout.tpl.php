368ce757b4eecd9710d10236d94a4456ce398258
Xabier Maeztu <trisketonni@gmail.com>
2015-12-10 09:07:23 +0100

    Adding layout for blog

---
+++ themes/coworker/layouts/blog/blog-layout.tpl.php
@@ -0,0 +1,15 @@
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <?php print $content['header'] ?>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-9">
+      <?php print $content['left'] ?>
+     </div>
+    <div class="col-md-3">
+      <?php print $content['right'] ?>
+    </div>
+  </div>
+</div>
---
+++ themes/coworker/views/views-view-fields--blog--blogs-by-cat.tpl.php
@@ -0,0 +1,1 @@
+views-view-fields--blog--page.tpl.php
