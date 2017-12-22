efe63f3ae94d410777a3f71a23797d06c7c550b0
Xabier Maeztu <xabier@katakrak.net>
2017-12-22 20:59:54 +0100

    Multiple changes

---
+++ themes/coworker/templates/layout-editorial.tpl.php
@@ -0,0 +1,78 @@
+<?php 
+  if ($page['sidebar_second']) {
+   $content_classes = 'col-md-9 col-lg-9 col-sm-12 col-xs-12'; 
+  }
+  else {
+    $content_classes = 'col-md-12 col-lg-12 col-sm-12 col-xs-12';
+  }
+?>
+
+<?php include 'page-header.inc' ?>
+<div id="content" class="book-page">
+  <div class="container">
+    <div class="content-wrap">
+    <?php if ($page['content_top']): ?>
+      <div id="content-top">
+        <?php print render($page['content_top']); ?>
+      </div>
+    <?php endif; ?>
+      </div>
+  </div>
+
+
+    
+      <div class="container clearfix">
+        <!-- content region -->
+        <div class="<?php print $content_class; ?> nobottommargin clearfix">
+          <?php if ($breadcrumb): ?>
+            <div id="breadcrumb"><?php //print $breadcrumb; ?></div>
+          <?php endif; ?>
+
+          <?php if ($messages): ?>
+            <?php print $messages; ?>
+          <?php endif; ?>
+
+          <?php if ($page['highlighted']): ?>
+            <div id="highlighted"><?php print render($page['highlighted']); ?></div>
+          <?php endif; ?>
+          
+          <a id="main-content"></a>
+          <h2 class="block"><?php print t($title); ?></h2>
+          
+          <?php if ($tabs): ?><div class="tabs"><?php //print render($tabs); ?></div><?php endif; ?>
+          <div class="row">
+            <div class="<?php print $content_classes ?>">
+              <?php print render($page['content']); ?>
+            </div>
+            <?php if ($page['sidebar_second']): ?>
+            <div class="col-md-3 col-lg-3 col-sm-12 col-xs-12">
+              <?php print render($page['sidebar_second']) ?>
+            </div>
+            <?php endif; ?>
+          </div>  
+          
+          <?php print render($page['content_bottom']) ?>
+        <?php if ($page['content_bottom_first'] || $page['content_bottom_second'] || $page['content_bottom_third'] || $page['content_bottom_fourth']): ?>
+        <div id="content-bottom" class="row">
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <?php print render($page['content_bottom_first']); ?>
+          </div>
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <?php print render($page['content_bottom_second']); ?>
+          </div>
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <?php print render($page['content_bottom_third']); ?>
+          </div>
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <?php print render($page['content_bottom_fourth']); ?>
+          </div>
+        </div>
+        <?php endif; ?>
+          <?php print $feed_icons; ?>
+        </div>
+        <!-- // content region -->
+
+    </div>
+  </div>
+
+<?php include 'page-footer.inc' ?>
