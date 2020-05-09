1aa86c9dae5c92ba349e4fc6a59119a239dd02eb
Xabier Maeztu <trisketonni@gmail.com>
2020-05-09 17:59:37 +0200

    Primeros pasos en los formularios de socios

---
+++ modules/custom/katakrak_socios/templates/katakrak-socios-alta-page.tpl.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
+      <?php print drupal_render($form_signup); ?>
+    </div>
+    <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
+      <?php print drupal_render($form_aportacion); ?>
+    </div>
+</div>
