3e95d813060cc5fd58a1127f68340c4fee440b8b
Xabier Maeztu <trisketonni@gmail.com>
2020-05-11 20:27:02 +0200

    Cambios a modulo socios

--- modules/custom/katakrak_socios/templates/katakrak-socios-alta-page.tpl.php
+++ modules/custom/katakrak_socios/templates/katakrak-socios-alta-page.tpl.php
@@ -1,8 +1,6 @@
 <div class="row">
-    <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
+    <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
       <?php print drupal_render($form_signup); ?>
-    </div>
-    <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
-      <?php print drupal_render($form_aportacion); ?>
+      <?php print $form_aportacion; ?>
     </div>
 </div>
