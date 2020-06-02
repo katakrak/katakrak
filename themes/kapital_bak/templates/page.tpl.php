d8ecb5a62a654754755dda11dddcafd87dd79021
Xabier Maeztu <trisketonni@gmail.com>
2020-06-02 19:45:35 +0200

    Changes

--- themes/kapital_bak/templates/page.tpl.php
+++ themes/kapital_bak/templates/page.tpl.php
@@ -1,97 +1,174 @@
-<?php include 'page-header.inc' ?>
-<main class="main">
-  
-<!-- Banner -->
-<div class="wrapper-banner">
-  <div class="container text-center">
-    <div class="row justify-content-md-center">
-      <div class="col-sm-6 col-lg-8">
-        <img src="/sites/all/themes/kapital/images/bansocix.gif" class="img-fluid" alt="Házte socix">
-      </div>
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display a single Drupal page.
+ *
+ * The doctype, html, head and body tags are not in this template. Instead they
+ * can be found in the html.tpl.php template in this directory.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - $base_path: The base URL path of the Drupal installation. At the very
+ *   least, this will always default to /.
+ * - $directory: The directory the template is located in, e.g. modules/system
+ *   or themes/bartik.
+ * - $is_front: TRUE if the current page is the front page.
+ * - $logged_in: TRUE if the user is registered and signed in.
+ * - $is_admin: TRUE if the user has permission to access administration pages.
+ *
+ * Site identity:
+ * - $front_page: The URL of the front page. Use this instead of $base_path,
+ *   when linking to the front page. This includes the language domain or
+ *   prefix.
+ * - $logo: The path to the logo image, as defined in theme configuration.
+ * - $site_name: The name of the site, empty when display has been disabled
+ *   in theme settings.
+ * - $site_slogan: The slogan of the site, empty when display has been disabled
+ *   in theme settings.
+ *
+ * Navigation:
+ * - $main_menu (array): An array containing the Main menu links for the
+ *   site, if they have been configured.
+ * - $secondary_menu (array): An array containing the Secondary menu links for
+ *   the site, if they have been configured.
+ * - $breadcrumb: The breadcrumb trail for the current page.
+ *
+ * Page content (in order of occurrence in the default page.tpl.php):
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title: The page title, for use in the actual HTML content.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ * - $messages: HTML for status and error messages. Should be displayed
+ *   prominently.
+ * - $tabs (array): Tabs linking to any sub-pages beneath the current page
+ *   (e.g., the view and edit tabs when displaying a node).
+ * - $action_links (array): Actions local to the page, such as 'Add menu' on the
+ *   menu administration interface.
+ * - $feed_icons: A string of all feed icons for the current page.
+ * - $node: The node object, if there is an automatically-loaded node
+ *   associated with the page, and the node ID is the second argument
+ *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
+ *   comment/reply/12345).
+ *
+ * Regions:
+ * - $page['help']: Dynamic help text, mostly for admin pages.
+ * - $page['highlighted']: Items for the highlighted content region.
+ * - $page['content']: The main content of the current page.
+ * - $page['sidebar_first']: Items for the first sidebar.
+ * - $page['sidebar_second']: Items for the second sidebar.
+ * - $page['header']: Items for the header region.
+ * - $page['footer']: Items for the footer region.
+ *
+ * @see bootstrap_preprocess_page()
+ * @see template_preprocess()
+ * @see template_preprocess_page()
+ * @see bootstrap_process_page()
+ * @see template_process()
+ * @see html.tpl.php
+ *
+ * @ingroup templates
+ */
+?>
+<header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
+  <div class="<?php print $container_class; ?>">
+    <div class="navbar-header">
+      <?php if ($logo): ?>
+        <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a>
+      <?php endif; ?>
+
+      <?php if (!empty($site_name)): ?>
+        <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+      <?php endif; ?>
+
+      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+          <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      <?php endif; ?>
     </div>
+
+    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+      <div class="navbar-collapse collapse" id="navbar-collapse">
+        <nav role="navigation">
+          <?php if (!empty($primary_nav)): ?>
+            <?php print render($primary_nav); ?>
+          <?php endif; ?>
+          <?php if (!empty($secondary_nav)): ?>
+            <?php print render($secondary_nav); ?>
+          <?php endif; ?>
+          <?php if (!empty($page['navigation'])): ?>
+            <?php print render($page['navigation']); ?>
+          <?php endif; ?>
+        </nav>
+      </div>
+    <?php endif; ?>
   </div>
-</div><!--/.banner -->
+</header>
 
-<?php if ($messages): ?>
-  <?php print $messages; ?>
+<div class="main-container <?php print $container_class; ?>">
+
+  <header role="banner" id="page-header">
+    <?php if (!empty($site_slogan)): ?>
+      <p class="lead"><?php print $site_slogan; ?></p>
+    <?php endif; ?>
+
+    <?php print render($page['header']); ?>
+  </header> <!-- /#page-header -->
+
+  <div class="row">
+
+    <?php if (!empty($page['sidebar_first'])): ?>
+      <aside class="col-sm-3" role="complementary">
+        <?php print render($page['sidebar_first']); ?>
+      </aside>  <!-- /#sidebar-first -->
+    <?php endif; ?>
+
+    <section<?php print $content_column_class; ?>>
+      <?php if (!empty($page['highlighted'])): ?>
+        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+      <?php endif; ?>
+      <?php if (!empty($breadcrumb)): print $breadcrumb;
+      endif;?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if (!empty($title)): ?>
+        <h1 class="page-header"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php if (!empty($tabs)): ?>
+        <?php print render($tabs); ?>
+      <?php endif; ?>
+      <?php if (!empty($page['help'])): ?>
+        <?php print render($page['help']); ?>
+      <?php endif; ?>
+      <?php if (!empty($action_links)): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+    </section>
+
+    <?php if (!empty($page['sidebar_second'])): ?>
+      <aside class="col-sm-3" role="complementary">
+        <?php print render($page['sidebar_second']); ?>
+      </aside>  <!-- /#sidebar-second -->
+    <?php endif; ?>
+
+  </div>
+</div>
+
+<?php if (!empty($page['footer'])): ?>
+  <footer class="footer <?php print $container_class; ?>">
+    <?php print render($page['footer']); ?>
+  </footer>
 <?php endif; ?>
-
-
-  <div class="row no-gutters">
-    <div class="col-sm-12 col-md-6">
-      <div class="box">
-        <img src="/sites/all/themes/kapital/images/tienda.jpg" class="img-fluid" alt="">
-      </div>
-    </div><!--/.col -->
-    <div class="col-sm-12 col-md-6">
-      <div class="box">
-        <img src="/sites/all/themes/kapital/images/espacio.jpg" class="img-fluid" alt="">
-      </div>
-    </div><!--/.col -->
-    <div class="col-sm-4">
-      <div class="box">
-        <img src="/sites/all/themes/kapital/images/cantina.jpg" class="img-fluid" alt="">
-      </div>
-    </div><!--/.col -->
-    <div class="col-sm-4">
-      <div class="box">
-          <img src="/sites/all/themes/kapital/images/editorial.jpg" class="img-fluid" alt="">
-      </div>
-    </div><!--/.col -->
-    <div class="col-sm-4">
-      <div class="box">
-        <img src="/sites/all/themes/kapital/images/agenda.jpg" class="img-fluid" alt="">
-      </div>
-    </div><!--/.col -->
-  </div><!--/.row -->
-
- 
-
-</main><!-- /.main -->
-
-<div class="wrapper-posts">
-  <div class="container">
-    <h1 class="h1-lg text-center">Ponte al día</h1>
-    <p class="text-center">Si quieres recibir estas noticias en tu email, <a href="https://xabiangos.com/katakrak-bs/index.html#">suscríbete aquí</a></p>
-      <div class="row mt-4">
-        <div class="col-md-4 col-sm-6">
-          <div class="post">
-           <div class="post-image">
-            <a href="https://xabiangos.com/katakrak-bs/index.html#"><img src="/sites/all/themes/kapital/images/tarkovski.jpg" class="img-fluid" alt=""></a>
-           </div>
-            <h3 class="post-title">
-              <a href="https://xabiangos.com/katakrak-bs/index.html#">¡Ya son más de 100! ¡Faltas tú!</a>
-            </h3>
-            <p class="small">Jueves 28 de mayo</p>
-          </div><!--/.post -->
-        </div><!--/.col -->
-        <div class="col-md-4 col-sm-6">
-          <div class="post">
-           <div class="post-image">
-            <a href="https://xabiangos.com/katakrak-bs/index.html#"><img src="/sites/all/themes/kapital/images/bici.jpg" class="img-fluid" alt=""></a>
-           </div>
-            <h3 class="post-title">
-              <a href="https://xabiangos.com/katakrak-bs/index.html#">Reparto en bici</a>
-            </h3>
-            <p class="small">Miércoles 27 de mayo</p>
-          </div><!--/.post -->
-        </div><!--/.col -->
-        <div class="col-md-4 col-sm-6">
-          <div class="post">
-           <div class="post-image">
-            <a href="https://xabiangos.com/katakrak-bs/index.html#"><img src="/sites/all/themes/kapital/images/diario.jpg" class="img-fluid" alt=""></a>
-           </div>
-            <h3 class="post-title">
-              <a href="https://xabiangos.com/katakrak-bs/index.html#">«Kapitalismoa eta emakumeen aurkako indarkeria» DIARIO VASCOn</a>
-            </h3>
-            <p class="small">Domingo 24 de mayo</p>
-          </div><!--/.post -->
-        </div><!--/.col -->
-      </div><!--/.row -->
-     <div class="text-center mt-3">
-      <button class="btn btn-secondary">Ver más posts</button>
-     </div>
-  </div><!--/.container -->
-</div><!--/.wrapper-posts -->
-<?php print render($page['content']); ?>
-<?php include 'page-footer.inc' ?>
