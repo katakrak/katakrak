4551f7d58925bf8899a111c467d428701efafbe2
Xabier Maeztu <trisketonni@gmail.com>
2020-06-05 13:15:45 +0200

    Various changes

--- themes/kapital/templates/page.tpl.php
+++ themes/kapital/templates/page.tpl.php
@@ -166,9 +166,4 @@
 
   </div>
 </div>
-
-<?php if (!empty($page['footer'])): ?>
-  <footer class="footer <?php print $container_class; ?>">
-    <?php print render($page['footer']); ?>
-  </footer>
-<?php endif; ?>
+<?php include 'page-footer.inc'; ?>
