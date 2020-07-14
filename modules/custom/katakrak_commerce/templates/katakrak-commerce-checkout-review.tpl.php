418c877d48ed743aa3e63c2011be8e0e465575c6
Xabier Maeztu <trisketonni@gmail.com>
2020-07-14 15:32:23 +0200

    Fixing issues with customer profiles

--- modules/custom/katakrak_commerce/templates/katakrak-commerce-checkout-review.tpl.php
+++ modules/custom/katakrak_commerce/templates/katakrak-commerce-checkout-review.tpl.php
@@ -16,11 +16,15 @@
       <h3 class="mt-0"><?php print t('Datos de envío') ?></h3>
       <p><?php print t('Nombre y apellidos:') ?><br>
       <strong><?php print $shipping->commerce_customer_address['und'][0]['name_line'] ?></strong></p>
+      <?php if ($shipping_method == 'recogida'): ?>
+      <strong><?php print t('Recogida en tienda') ?></strong>
+      <?php else: ?>
       <p><?php print t('Dirección') ?><br>
       <strong><?php print $shipping->commerce_customer_address['und'][0]['thoroughfare'] ?> </strong><br>
       <?php print $shipping->commerce_customer_address['und'][0]['postal_code'] ?> <?php print $shipping->commerce_customer_address['und'][0]['locality'] ?>, <?php print $shipping->commerce_customer_address['und'][0]['administrative_area'] ?> </p>
       <p><?php print t('Teléfono:') ?><br>
       <strong><?php print $billing->field_customer_telefono['und'][0]['value'] ?></strong></p>
+      <?php endif; ?>
     </div><!-- /.well-->
   </div><!-- /.col-6 -->
 </div><!-- /.row -->
--- modules/custom/katakrak_commerce/templates/katakrak-commerce-informacion-envio.tpl.php
+++ modules/custom/katakrak_commerce/templates/katakrak-commerce-informacion-envio.tpl.php
@@ -1,6 +1,6 @@
 <div class="small">
 <?php foreach($shipping_methods as $method): ?>
-  <?php //dpm($method) ?>
+  
   <div class="tipo-envio text-gray <?php print $method->data['shipping_service']['name'] ?>">
     <h4><?php print t($method->data['shipping_service']['display_title'])?></h4>
     <p><?php print t($method->data['shipping_service']['description'])?></p>
