5809aa9215a85817e1a44a51556cb2badc216e3b
Xabier Maeztu <trisketonni@gmail.com>
2020-10-30 16:03:47 +0100

    Changes in banner socixs

--- modules/custom/katakrak_home/templates/katakrak-home-banner.tpl.php
+++ modules/custom/katakrak_home/templates/katakrak-home-banner.tpl.php
@@ -3,7 +3,7 @@
   <div class="container text-center">
     <div class="row">
       <div class="col-sm-6 col-sm-offset-3 col-lg-8 col-lg-offset-2">
-        <img src="/sites/all/themes/kapital/images/bansocix.gif" class="img-responsive" alt="Házte socix">
+          <a href="<?php print url($path)?>"><img src="/sites/all/themes/kapital/images/bansocix.gif" class="img-responsive" alt="<?php print t('Házte socix') ?>"></a>
       </div>
     </div>
   </div>
