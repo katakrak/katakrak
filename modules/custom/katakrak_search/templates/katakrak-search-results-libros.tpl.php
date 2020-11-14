b0f1969201152ff43d8eec0d213b626f33ae5e01
Xabier Maeztu <trisketonni@gmail.com>
2020-11-14 16:23:16 +0100

    Changing search mode. Later we can implement different themes for different search types

---
+++ modules/custom/katakrak_search/templates/katakrak-search-results-libros.tpl.php
@@ -0,0 +1,16 @@
+<?php
+/* 
+ * Recogemos todos los resultados de la búsqueda y mostramos la vista cardbook. 
+ * No es la solución mas elegante, pero funciona
+ */
+foreach ($results as $result) {
+    $nids[] = $result['node']->entity_id;
+  }
+  $nodes = node_load_multiple($nids);
+  foreach ($nodes as $node) {
+    $node_view = node_view($node, 'card_book_md');
+    $output .= drupal_render($node_view);
+  }
+  print $output;
+
+
