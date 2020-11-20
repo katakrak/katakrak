0bd6cfd465c240b8a24053fa9f2d43fdb0bcdb47
Xabier Maeztu <trisketonni@gmail.com>
2020-11-20 20:04:15 +0100

    Changes to tienda online

--- modules/custom/libro/templates/libreria-home-destacado.tpl.php
+++ modules/custom/libro/templates/libreria-home-destacado.tpl.php
@@ -1,5 +1,30 @@
-    <div class="modulo-destacado">
+<div class="modulo-destacado">
       <h2 class="text-center">¿Cómo es la compra online en Katakrak?</h2>
 <p class="text-center">Trabajamos por una compra cercana y sostenible</p>
-<p>ilustraciones: pago seguro, entrega bizikrak, trato cercano</p>
+      <div class="row">
+        <div class="col-sm-6 col-sm-offset-3">
+          <div class="row">
+            <div class="col-sm-4">
+              <img src="/sites/all/themes/kapital/images/pago-seguro.png" class="img-responsive" alt="Compra segura">
+              <p class="text-center mt-2">
+                <strong>Compra segura</strong><br>Desarrollar
+              </p>
+              
+            </div><!-- /.col -->
+            <div class="col-sm-4">
+              <img src="/sites/all/themes/kapital/images/bizikrak.png" class="img-responsive" alt="Bizikrak">
+              <p class="text-center mt-2">
+                <strong>Entrega sostenible</strong><br>Desarrollar
+              </p>
+            </div><!-- /.col -->
+            <div class="col-sm-4">
+              <img src="/sites/all/themes/kapital/images/trato-cercano.png" class="img-responsive" alt="Trato cercano">
+              <p class="text-center mt-2">
+                <strong>Trato cercano</strong><br>Desarrollar
+              </p>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div>
+      </div>
     </div>
+<hr class="hr-dark">
