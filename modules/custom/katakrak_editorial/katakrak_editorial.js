8e3894bb490161b3d8db1aff279594207f3c4072
Xabier Maeztu <trisketonni@gmail.com>
2020-11-27 13:53:45 +0100

    Changes to editorial menu

--- modules/custom/katakrak_editorial/katakrak_editorial.js
+++ modules/custom/katakrak_editorial/katakrak_editorial.js
@@ -2,7 +2,6 @@
   Drupal.behaviors.katakrak_editorial = {
     attach: function (context, settings) {
       $('main.main').css('background-color', $('.color-fondo').html());
-      console.log($('.color-fondo').html());
     }
   };
 })(jQuery);
