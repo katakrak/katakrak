4f0c8ba28c89213e3c22ee3c1cb8fc95d887aaa6
Xabier Maeztu <trisketonni@gmail.com>
2020-11-28 17:15:49 +0100

    Updating autores editorial.

--- modules/custom/katakrak_editorial/katakrak_editorial.js
+++ modules/custom/katakrak_editorial/katakrak_editorial.js
@@ -2,6 +2,11 @@
   Drupal.behaviors.katakrak_editorial = {
     attach: function (context, settings) {
       $('main.main').css('background-color', $('.color-fondo').html());
+       $('.imagen-autor-ed  img').each(function(index) {
+          console.log($(this));
+          $(this).addClass('img-circle');
+       });
     }
+    
   };
 })(jQuery);
