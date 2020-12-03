af3b70a365d680b8696c32f61c5cd8f25908a77f
Xabier Maeztu <trisketonni@gmail.com>
2020-12-03 18:47:58 +0100

    Landing for agenda

---
+++ modules/custom/katakrak_agenda/templates/katakrak-agenda-landing-baldosas.tpl.php
@@ -0,0 +1,30 @@
+<div class="row">
+      <div class="col-sm-6 col-md-4">
+        <a href="#" class="features">
+          <p>
+            <img src="images/sala-presentacion.jpg" class="img-responsive" alt="Presenta tu libro en Katakrak">
+          </p>
+          <h3>Presenta tu libro</h3>
+          <p> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+        </a>
+      </div>
+      <div class="col-sm-6 col-md-4">
+        <a href="#" class="features">
+          <p>
+            <img src="images/sala-concierto.jpg" class="img-responsive" alt="Organiza tu concierto en Katakrak">
+          </p>
+          <h3>Programa tu concierto</h3>
+          <p> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+        </a>
+      </div>
+      <div class="col-sm-6 col-md-4">
+        <a href="#" class="features">
+          <p>
+            <img src="images/sala-charla.jpg" class="img-responsive" alt="Organiza tu charla en Katakrak">
+          </p>
+          <h3>Un espacio par tu charla</h3>
+          <p> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+        </a>
+      </div>
+      
+    </div>
---
+++ modules/custom/katakrak_agenda/templates/katakrak-agenda-landing-visitado.tpl.php
@@ -0,0 +1,42 @@
+<div class="mt-2">
+        <h2>Nos han visitado</h2>
+        
+          <div id="carousel-cantina" class="carousel slide mt-2" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#sala1" data-slide-to="0" class="active"></li>
+              <li data-target="#sala2" data-slide-to="1"></li>
+            </ol>
+          
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+              <div class="item active" id="sala1">
+                  <img src="/sites/all/themes/kapital/images/silvia-federici.jpg" alt="Katakrak Cantina">
+                <div class="carousel-caption">
+                  <h3>Silvia Federici</h3>
+                  <p>Lorem ipsum</p>
+                </div>
+              </div><!-- item-active -->
+              <div class="item" id="sala2">
+                <img src="/sites/all/themes/kapital/images/kyanga.jpg" alt="Katakrak Cantina">
+                <div class="carousel-caption">
+                  <h3>Kyanga Taylor</h3>
+                  <p>Lorem ipsum</p>
+                </div>
+              </div><!-- item-active -->
+              
+
+            </div><!-- carousel-inner -->
+          
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-cantina" role="button" data-slide="prev">
+              <img src="/sites/all/themes/kapital/images/angle-left.svg" width="30" height="30" alt="Anterior">
+            </a>
+            <a class="right carousel-control" href="#carousel-cantina" role="button" data-slide="next">
+              <img src="/sites/all/themes/kapital/images/angle-right.svg" width="30" height="30" alt="Siguiente">
+            </a>
+          </div>
+      
+
+  
+       </div>
