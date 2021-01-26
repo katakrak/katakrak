6e40dd7528fb3e0c2427972ad36f66271cdad3af
Xabier Maeztu <trisketonni@gmail.com>
2021-01-26 13:32:26 +0100

    Updating kantina module

---
+++ themes/kapital/views/kantina/views-view-fields--kantina-productores--attachment.tpl.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to all the fields as a row.
+ *
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ *   - $field->inline: Whether or not the field should be inline.
+ *   - $field->inline_html: either div or span based on the above flag.
+ *   - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
+ *   - $field->wrapper_suffix: The closing tag for the wrapper.
+ *   - $field->separator: an optional separator that may appear before a field.
+ *   - $field->label: The wrap label text to use.
+ *   - $field->label_html: The full HTML of the label to use including
+ *     configured element type.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
+ * @ingroup views_templates
+ */
+//dpm($fields)
+?>
+<div class="modal fade" id="modalProductora-<?php print $fields['nid']->raw?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel"><?php print $fields['title']->content ?></h4>
+      </div>
+      <div class="modal-body">
+        <?php print $fields['field_image']->content ?>
+        <?php print $fields['body']->content ?>
+        <?php if ($fields['field_proveedor_link']->content): ?>
+        <p>
+          <a class="btn btn-secondary" href="<?php print $fields['field_proveedor_link']->content ?>" target="_blank"><?php print t('Visitar su web') ?></a>
+        </p>
+        <?php endif; ?>
+      </div>
+    </div>
+  </div>
+</div>
