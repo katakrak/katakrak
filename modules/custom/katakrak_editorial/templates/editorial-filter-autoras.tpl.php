099d803ff03e61acd1681e041b9bee6c3899a54a
Xabier Maeztu <trisketonni@gmail.com>
2021-02-10 11:32:35 +0100

    updating editorial

--- modules/custom/katakrak_editorial/templates/editorial-filter-autoras.tpl.php
+++ modules/custom/katakrak_editorial/templates/editorial-filter-autoras.tpl.php
@@ -1,1 +1,21 @@
-<p><input type="text" class="quicksearch" placeholder="Search" /></p>
+<div class="col-sm-8 col-sm-offset-2">
+  <div class="row mt-2">
+    <div class="col-sm-6">
+      <div class="form-group has-feedback">
+        <input type="text" class="form-control quicksearch" placeholder="<?php print t('Nombre de autor')?>">
+        <span class="form-control-feedback" aria-hidden="true">
+          <i class="fas fa-search" aria-hidden="true"></i>
+        </span>
+      </div>
+    </div>
+    <div class="col-sm-6">
+                   <div class="form-group">
+                    
+                     <select class="form-control">
+                         <option selected="">Todos</option>
+                         <option>Escfitores</option>
+                         <option>Traductores</option>
+                       </select>
+                     </div><!-- /.form-group-->
+                 </div>
+  </div>
