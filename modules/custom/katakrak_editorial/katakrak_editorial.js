ea548ea9b94c66c2bc10f846dd2383726e2ec31e
Xabier Maeztu <trisketonni@gmail.com>
2021-02-10 10:30:44 +0100

    Stupp

--- modules/custom/katakrak_editorial/katakrak_editorial.js
+++ modules/custom/katakrak_editorial/katakrak_editorial.js
@@ -3,9 +3,41 @@
     attach: function (context, settings) {
       $('main.main').css('background-color', $('.color-fondo').html());
        $('.imagen-autor-ed  img').each(function(index) {
-          console.log($(this));
           $(this).addClass('img-circle');
        });
+       
+       // quick search regex
+        var qsRegex;
+
+        // init Isotope
+        var $grid = $('.grid').isotope({
+          itemSelector: '.col',
+          layoutMode: 'fitRows',
+          filter: function() {
+            return qsRegex ? $(this).text().match( qsRegex ) : true;
+          }
+        });
+
+        // use value of search field to filter
+        var $quicksearch = $('.quicksearch').keyup( debounce( function() {
+          qsRegex = new RegExp( $quicksearch.val(), 'gi' );
+          $grid.isotope();
+        }, 200 ) );
+
+        // debounce so filtering doesn't happen every millisecond
+        function debounce( fn, threshold ) {
+          var timeout;
+          threshold = threshold || 100;
+          return function debounced() {
+            clearTimeout( timeout );
+            var args = arguments;
+            var _this = this;
+            function delayed() {
+              fn.apply( _this, args );
+            }
+            timeout = setTimeout( delayed, threshold );
+          };
+        }
     }
     
   };
