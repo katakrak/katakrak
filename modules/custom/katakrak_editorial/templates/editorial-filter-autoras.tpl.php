02785afe6a6800685d13d442cc2f8a62573cd53e
Xabier Maeztu <trisketonni@gmail.com>
2021-02-17 11:56:03 +0100

    Filter isotope in both traductor and autor

--- modules/custom/katakrak_editorial/templates/editorial-filter-autoras.tpl.php
+++ modules/custom/katakrak_editorial/templates/editorial-filter-autoras.tpl.php
@@ -1,6 +1,6 @@
 <div class="col-sm-8 col-sm-offset-2">
   <div class="row mt-2">
-    <div class="col-sm-6">
+    <div class="col-sm-6 col-sm-offset-3">
       <div class="form-group has-feedback">
         <input type="text" class="form-control quicksearch" placeholder="<?php print t('Nombre de autor')?>">
         <span class="form-control-feedback" aria-hidden="true">
@@ -8,14 +8,5 @@
         </span>
       </div>
     </div>
-    <div class="col-sm-6">
-      <div class="form-group">
-
-        <select class="form-control filter-autores-traductores">
-            <option selected="">Todos</option>
-            <option>Escfitores</option>
-            <option>Traductores</option>
-          </select>
-        </div><!-- /.form-group-->
-    </div>
   </div>
+</div>
