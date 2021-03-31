1c5e35d9356db96cfa3b6e8367c8c1d56cfe82d2
Xabier Maeztu <trisketonni@gmail.com>
2021-03-31 10:03:27 +0200

    Home fix

--- themes/kapital/views/blog/views-view-unformatted--blog--blog-home.tpl.php
+++ themes/kapital/views/blog/views-view-unformatted--blog--blog-home.tpl.php
@@ -10,12 +10,12 @@
   <div class="container">
     <h1 class="h1-lg text-center"><?php print t('Ponte al día') ?></h1>
     <p class="text-center"><?php print t('Si quieres recibir estas noticias en tu email, <a href="#">suscríbete aquí</a>' )?></p>
-    <div class="" data-aos="fade-up">
+    <div class="row" data-aos="fade-up">
       <?php foreach ($rows as $id => $row): ?>
           <?php print $row; ?>
       <?php endforeach; ?>
     </div><!--/.row mt-4 -->
-    <div class="text-center mt-3">
+    <div class="text-center mt-3 mb-3">
       <?php print l(t('Ver más posts'), 'blogs', array('attributes' => array('class' => array('btn btn-secondary')))) ?>
     </div>
     
