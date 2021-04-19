34b3aaa98adc0371b6f2c85675c0b3ba23c389ad
Xabier <trisketonni@gmail.com>
2021-04-19 16:11:33 +0200

    small change

--- modules/custom/katakrak_home/templates/katakrak-home-kantina.tpl.php
+++ modules/custom/katakrak_home/templates/katakrak-home-kantina.tpl.php
@@ -1,9 +1,9 @@
 <div class="cta cta-secondary cta-katering">
   <div class="cta-text">
     <div>
-      <h1 class="h1-lg text-color-light"><?php print t('Buen comer...') ?></h1>
-      <p class="lead text-color-light"><?php print t('y buen beber') ?></p>
+      <h1 class="h1-lg text-color-light"><?php print t('La cantina de Katakrak') ?></h1>
+      <p class="lead text-color-light"><?php print t('Una apuesta de hostelería desde la soberanía alimentaria') ?></p>
     </div>
-    <a class="btn btn-light" href="<?php print url('cantina')?>"><?php print t('Cantina') ?></a>
+    <a class="btn btn-light" href="<?php print url('cantina')?>"><?php print t('Conoce más') ?></a>
   </div><!--/.cta-text -->
 </div><!--/.cta-katering -->
