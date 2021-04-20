824dc39de4f034fd0949f44871c897a58fb37fa3
Xabier <trisketonni@gmail.com>
2021-04-20 11:01:13 +0200

    Fixes

--- modules/custom/katakrak/templates/katakrak-footer-first.tpl.php
+++ modules/custom/katakrak/templates/katakrak-footer-first.tpl.php
@@ -10,5 +10,5 @@
 <ul class="list-unstyled">
   <li class="list-style"><?php print t('Abierto de lunes a sábado') ?></li>
   <li class="list-item"> <span class="strong"><?php print t('Librería') ?>: </span> 10:00-14:00 17:00-20:30</li>
-  <li class="list-item"><span class="strong"><?php print t('Cantina') ?>: </span>  L,M,M 10:00-22:00 | J,V,S 10:00-01:00</li>
+  <li class="list-item"><span class="strong"><?php print t('Cantina') ?>: </span>  L, M, M 10:00-22:00 | J, V, S 10:00-01:00</li>
 </ul>
