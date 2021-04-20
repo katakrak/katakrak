daf2e136cd31c0d1574f562b35af701f9c67eeae
Xabier <trisketonni@gmail.com>
2021-04-20 16:42:34 +0200

    Fixes

--- modules/custom/katakrak_agenda/templates/katakrak-agenda-landing-destacado.tpl.php
+++ modules/custom/katakrak_agenda/templates/katakrak-agenda-landing-destacado.tpl.php
@@ -3,8 +3,7 @@
   <div class="col-sm-6 col-flex bg-blue-p" id="reservar">
     
     <h1 class="h1-lg"><?php print t('Organiza un acto en Katakrak') ?></h1>
-    <p class="lead lead-hero-2 mt-2"><?php print t('Colectivos, personas a título individual, fundaciones y otro tipo de combinaciones sociales organizan actos.
-      Tú también puedes hacerlo.') ?></p>
+    <p class="lead lead-hero-2 mt-2"><?php print t('Colectivos, personas a título individual, fundaciones y otro tipo de combinaciones sociales organizan actos. Tú también puedes hacerlo.') ?></p>
     <p>
         <img src="/sites/all/themes/kapital/images/plano-sala.png" style="width: 250px;" alt="Sala de actos de Katakrak">
     </p>
