24544775f39aec19012a14640bcf4d1161ae6e3c
Xabier <trisketonni@gmail.com>
2021-04-20 12:52:10 +0200

    Small changes

--- modules/custom/katakrak_home/templates/katakrak-home-editorial.tpl.php
+++ modules/custom/katakrak_home/templates/katakrak-home-editorial.tpl.php
@@ -1,8 +1,8 @@
 <div class="cta cta-secondary cta-editorial">
   <div class="cta-text">
     <div>
-      <h1 class="title-light"><?php print t('Elegimos, traducimos, compartimos <b>Editamos</b>') ?></h1>
+      <h1 class="h1-lg text-color-light"><?php print t('Elegimos, traducimos, compartimos <b>Editamos</b>') ?></h1>
     </div>
-    <a class="btn btn-dark" href="<?php print url('editorial') ?>"><?php print t('Editorial') ?></a>
+    <a class="btn btn-light" href="<?php print url('editorial') ?>"><?php print t('Editorial') ?></a>
   </div><!--/.cta-text -->
 </div><!--/.cta-katering -->
