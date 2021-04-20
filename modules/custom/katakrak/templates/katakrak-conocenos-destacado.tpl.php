161e9f1b9070aed8598510dda3f6825f6fc7a7ca
Xabier <trisketonni@gmail.com>
2021-04-20 16:57:08 +0200

    Various

--- modules/custom/katakrak/templates/katakrak-conocenos-destacado.tpl.php
+++ modules/custom/katakrak/templates/katakrak-conocenos-destacado.tpl.php
@@ -5,7 +5,7 @@
     <p class="lead lead-hero-2 mt-2"><?php print t('Somos una librería, una cantina, una editorial, un espacio para eventos... y mucho más') ?></p>
     <!--<p class="lead lead-hero">Segundo bloque de texto</p>-->
     <p class="mt-2">
-        <a class="btn btn-primary" href="#visita-virtual">Visita virtual</a>
+      <a class="btn btn-primary" href="#visita-virtual"><?php print t('Visita virtual') ?></a>
     </p>
 </div>
 </div>
