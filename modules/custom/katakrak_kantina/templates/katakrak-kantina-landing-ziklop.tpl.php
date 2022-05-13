6faf967015b51a72247d3d7fd12dc8e2eaff5699
Xabier <trisketonni@gmail.com>
2022-05-13 16:25:16 +0200

    Para llevar page

---
+++ modules/custom/katakrak_kantina/templates/katakrak-kantina-landing-ziklop.tpl.php
@@ -0,0 +1,22 @@
+<div class="row mt-2">
+        <div class="col-sm-6 mb-1">
+            <img src="/sites/all/themes/kapital/images/kantina-ziclop.jpg" alt="Ziclop" class="img-responsive">
+        </div>
+      <div class="col-sm-6">
+        <h1><?php print t('Comida para llevar') ?></h1>
+       <p class="lead">
+           <?php print t('Ziclo-P es una microcooperativa de ciclo-logística de Pamplona-Iruña.') ?>
+       </p>
+       <p>
+        <?php print t('Es la alternativa local y ética a las grandes plataformas de reparto a domicilio. Nace con el objetivo de reivindicar la protección de los derechos laborales de las personas trabajadoras, el fomento de la bicicleta como medio transporte urbano y sostenible y la apuesta por un nuevo modelo socioeconómico, con base en el comercio local y el consumo responsable.') ?>
+       </p>
+       <p>
+         <?php print t('Los principales servicios de la cooperativa son el reparto de comida a domicilio, la logística del comercio local y el reparto de última milla.') ?>.
+       </p>
+       <p>
+         <button class="btn btn-primary" type="button" data-coopcycle-embed="https://ziclop.coopcycle.org/es/restaurant/7-katakrak">
+        <?php print t('Haz tu pedido') ?> 
+        </button>
+       </p>
+      </div><!-- /.col-->
+    </div><!-- /.row-->
