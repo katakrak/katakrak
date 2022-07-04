32a828ca9349a9d08b580fab711ab6494a2ac951
Xabier <trisketonni@gmail.com>
2022-07-04 17:12:51 +0200

    Change

--- modules/custom/katakrak_commerce/templates/katakrak-commerce-checkout-breadcrumb.tpl.php
+++ modules/custom/katakrak_commerce/templates/katakrak-commerce-checkout-breadcrumb.tpl.php
@@ -10,3 +10,8 @@
   <?php endif; ?>
  <?php endforeach; ?>
 </ul>
+<div class="banner-bizikrak">
+  <a href="#">
+    <i class="fa fa-warning" aria-hidden="true"></i> <?php print t('<strong>Tienda onlince cerrada</strong> Del 5 al 17 de julio la librería está cerrada, tanto de forma presencial como online.') ?>
+  </a>
+</div>
