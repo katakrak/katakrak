069d843b82b40d0410fd1226d5530f33356396b5
Xabier <trisketonni@gmail.com>
2022-07-04 17:04:16 +0200

    Banner

--- modules/custom/libro/templates/libreria-home-bizikrak.tpl.php
+++ modules/custom/libro/templates/libreria-home-bizikrak.tpl.php
@@ -1,5 +1,11 @@
-<div class="banner-bizikrak">
+<!--<div class="banner-bizikrak">
   <a href="#">
     <i class="fa fa-bicycle" aria-hidden="true"></i> <?php print t('<strong>Bizikrak.</strong> Te llevamos tus libros de forma sostenible en 48h') ?>
+  </a>
+</div> -->
+
+<div class="banner-bizikrak">
+  <a href="#">
+    <i class="fa fa-warning" aria-hidden="true"></i> <?php print t('<strong>Tienda onlince cerrada</strong> Del 5 al 17 de julio la librería está cerrada, tanto de forma presencial como online.') ?>
   </a>
 </div>
---
+++ modules/custom/libro/templates/libreria-home-cerrada.tpl.php
@@ -0,0 +1,5 @@
+<div class="banner-bizikrak">
+  <a href="#">
+    <i class="fa fa-warning" aria-hidden="true"></i> <?php print t('<strong>Tienda onlince cerrada</strong> Del 5 al 17 de julio la librería está cerrada, tanto de forma presencial como online.') ?>
+  </a>
+</div>
