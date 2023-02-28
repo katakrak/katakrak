cef0d2226cdcabba553c23cc8aa2d0552213fc3a
Juan Etxenike Almeida <xaresd@gmail.com>
2023-02-28 18:50:14 +0100

    Add picture for the list of courses card

--- modules/custom/katakrak_nncc/templates/katakrak-nncc-page-hero.tpl.php
+++ modules/custom/katakrak_nncc/templates/katakrak-nncc-page-hero.tpl.php
@@ -1,5 +1,5 @@
 <div class="d-flex-fullwidth">
-  <div class=" col-sm-6 bg-image bg-dark bg-nncc col-flex hero-img-empty"></div>
+  <div class="col-sm-6 bg-image bg-dark bg-nncc col-flex hero-img-empty"></div>
   <div class="col-sm-6 col-flex bg-yellow-p" id="reservar">
     <h1 class="h1-lg"><?php print t('Centro de Estudios') ?></h1>
     <p class="lead lead-hero-2 mt-2"><?php print t('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu lacinia arcu. Morbi venenatis eleifend metus, ut aliquet purus varius finibus. Proin orci lacus, hendrerit quis ornare eu, congue et lacus.') ?></p>
@@ -8,7 +8,7 @@
     </p>
     <p><?php print t('Proin id diam non est aliquet egestas congue lobortis dolor. Ut lacinia dolor vel aliquet ullamcorper. Sed volutpat eleifend dui, eu dignissim lacus consectetur vitae.') ?></p>
     <p class="lead lead-hero mt-2">
-      <a class="btn btn-primary" href="<?php print url('editorial/catalogo') ?>"><?php print t('Ver catálogo') ?></a>
+      <a class="btn btn-primary" href="<?php print url('ciclos/cursos') ?>"><?php print t('Ver cursos') ?></a>
     </p>
 
   </div>
