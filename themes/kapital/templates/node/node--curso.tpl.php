ad4214ece1e868cc79911ac40731bbf2bb570e87
Juan Etxenike Almeida <xaresd@gmail.com>
2023-03-09 05:44:45 +0100

    Add node--curso view in theme

---
+++ themes/kapital/templates/node/node--curso.tpl.php
@@ -0,0 +1,37 @@
+<p>
+  <a href="<?php print url('agenda') ?>" class="btn btn-transparent"> <i class="fas fa-arrow-left small"></i><?php print t('Listado de eventos') ?></a>
+</p>
+<div class="row mt-2">
+  <div class="col-sm-4">
+    <?php print render($content['field_image']) ?>
+    <hr class="hr-dark">
+
+    <a href="<?php echo render($content['field_url_denda']['#items'][0]['value']); ?>"><?php print t('Apuntarse'); ?></a>
+    <hr class="hr-dark">
+    <h3 class="mb-2"><?php print t('Producto relacionado') ?></h3>
+     <?php print render($content['field_producto_ciclo']) ?>
+    </div><!-- /.col-->
+        <div class="col-sm-8">
+          <div class="d-flex event-title">
+            <div>
+              <h1 class="mt-0"><?php print $node->title ?></h1>
+             <p class="lead"><?php print render($content['field_ciclo_descripci_n']) ?></p>
+
+            </div>
+          </div>
+        <hr>
+
+        <!--TODO<h4>Compártelo:</h4>
+        <p>RRSS</p>-->
+        </div><!-- /.col-->
+
+
+
+    </div><!-- /.row -->
+    <hr class="hr-dark">
+    <h3 class="mb-2"><?php print t('Eventos relacionados') ?></h3>
+     <?php print views_embed_view('agenda', 'citas_ciclo', $node->nid) ?>
+
+    <hr class="hr-dark">
+    <h3 class="mb-2"><?php print t('Libros relacionados') ?></h3>
+     <?php print render($content['field_ciclo_libros']) ?>
