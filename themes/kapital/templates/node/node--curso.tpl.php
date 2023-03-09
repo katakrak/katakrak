19a7f3e8613efe980371002a1b9c3fe5c56b40d2
Juan Etxenike Almeida <xaresd@gmail.com>
2023-03-09 05:52:55 +0100

    Update node--curso view in theme

--- themes/kapital/templates/node/node--curso.tpl.php
+++ themes/kapital/templates/node/node--curso.tpl.php
@@ -3,19 +3,19 @@
 </p>
 <div class="row mt-2">
   <div class="col-sm-4">
-    <?php print render($content['field_image']) ?>
+    <?php print render($content['field_curso_imagen']) ?>
     <hr class="hr-dark">
 
     <a href="<?php echo render($content['field_url_denda']['#items'][0]['value']); ?>"><?php print t('Apuntarse'); ?></a>
     <hr class="hr-dark">
-    <h3 class="mb-2"><?php print t('Producto relacionado') ?></h3>
-     <?php print render($content['field_producto_ciclo']) ?>
+    <!-- <h3 class="mb-2"><?php print t('Producto relacionado') ?></h3>-->
+     <?php //print render($content['field_producto_ciclo']) ?>
     </div><!-- /.col-->
         <div class="col-sm-8">
           <div class="d-flex event-title">
             <div>
               <h1 class="mt-0"><?php print $node->title ?></h1>
-             <p class="lead"><?php print render($content['field_ciclo_descripci_n']) ?></p>
+             <p class="lead"><?php print render($content['field_curso_descripcion']) ?></p>
 
             </div>
           </div>
@@ -34,4 +34,4 @@
 
     <hr class="hr-dark">
     <h3 class="mb-2"><?php print t('Libros relacionados') ?></h3>
-     <?php print render($content['field_ciclo_libros']) ?>
+     <?php print render($content['field_curso_libros']) ?>
