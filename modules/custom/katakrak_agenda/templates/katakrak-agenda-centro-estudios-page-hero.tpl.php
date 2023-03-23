ada53d00a4abd5ab906c8c65cb1f0e8212be7595
Juan Etxenike Almeida <xaresd@gmail.com>
2023-03-23 15:22:11 +0100

    Change texts for centro de estudios

--- modules/custom/katakrak_agenda/templates/katakrak-agenda-centro-estudios-page-hero.tpl.php
+++ modules/custom/katakrak_agenda/templates/katakrak-agenda-centro-estudios-page-hero.tpl.php
@@ -2,7 +2,10 @@
   <div class=" col-sm-6 bg-image bg-dark bg-nncc col-flex hero-img-empty"></div>
   <div class="col-sm-6 col-flex bg-yellow-p" id="reservar">
     <h1 class="h1-lg"><?php print t('Centro de Estudios') ?></h1>
-    <p class="lead lead-hero-2 mt-2"><?php print t('Desde Katakrak apostamos por generar una estructura dedicada a la formación y la investigación. En la práctica: aumentar y mejorar nuestra oferta de cursos y grupos de lectura, escribir y publicar textos de investigación militante.') ?></p>
+    <p class="lead lead-hero-2 mt-2"><?php print t('Una pequeña estructura dedicada a la formación y la investigación.') ?></p>
+    <p>
+      <?php print t('Dos fines: de un lado, generar una oferta permantente de cursos y grupos de lectura; del otro, escribir y publicar textos de investigación militante.') ?>
+    </p>
     <p>
       <?php print t('Una labor conectada a una red amplia de centros sociales, librerías y editoriales políticas que llevan años trabajando por la formación política y la puesta en común de saberes útiles para la emancipación social.') ?>
     </p>
