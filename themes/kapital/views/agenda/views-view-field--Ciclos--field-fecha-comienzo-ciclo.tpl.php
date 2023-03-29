289c1544cf59a9dc21f16a43328af6af17b5eb19
Juan Etxenike Almeida <xaresd@gmail.com>
2023-03-29 08:54:08 +0200

    Fix ciclos date

--- themes/kapital/views/agenda/views-view-field--Ciclos--field-fecha-comienzo-ciclo.tpl.php
+++ themes/kapital/views/agenda/views-view-field--Ciclos--field-fecha-comienzo-ciclo.tpl.php
@@ -41,7 +41,7 @@ $year_suffix = (substr($start_year, -1) == '1'
 || substr($start_year, -2) == '50'
 || substr($start_year, -2) == '70'
 || substr($start_year, -2) == '90') ? '\ek\o' : 'k\o';
-$day_suffix = (in_array($start_day, [5, 10, 30])) ? '\e' : '';
+$day_suffix = (in_array($start_day, [1, 5, 10, 30])) ? '\e' : '';
 $month_suffix = '\r\e\n';
 $date_format_start = "Y{$year_suffix} F{$month_suffix} j{$day_suffix}";
 
@@ -52,7 +52,7 @@ $year_suffix = (substr($end_year, -1) == '1'
 || substr($end_year, -2) == '50'
 || substr($end_year, -2) == '70'
 || substr($end_year, -2) == '90') ? '\ek\o' : 'k\o';
-$day_suffix = (in_array($end_day, [5, 10, 30])) ? '\e' : '';
+$day_suffix = (in_array($end_day, [1, 5, 10, 30])) ? '\e' : '';
 
 if( $start_year == $end_year ) {
   $date_format_end = "F{$month_suffix} j{$day_suffix}";
