dd990f59e52fd475aa2f971b996ba87dfa0d2152
xare <xaresd@gmail.com>
2023-10-19 12:52:41 +0200

    Add user mail dump again

--- themes/kapital/templates/node/node--disco.tpl.php
+++ themes/kapital/templates/node/node--disco.tpl.php
@@ -16,7 +16,7 @@
     //Si el usuario está logueado deberíamos mostrar el boton de compra del producto con el precio normal. O el producto con precio rebajado si es socixs. Y un boton de hazte socix
     //Si no está logueado, habría que poner el producto normal y un botón de hazte socixs o que si lo eres te lleve a loguearte.
     //Habrá que comprobar si el usuario es socixs. Por ejemplo, guardando la lista de socixs en una variable de drupal y comprobando el email. ?>
-
+  <?php var_dump( $user->mail ); ?>
     <?php var_dump( variable_get( 'lista_socixs', '' ) ); ?>
     <?php var_dump( strpos( variable_get( 'lista_socixs', '' ), $user->mail) ); ?>
     <?php if ($user->uid == 1): ?>
