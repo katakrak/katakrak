4041c9d3f478d8da27167681d66f96b74963a7f0
xare <xaresd@gmail.com>
2023-10-19 12:44:55 +0200

    Add user mail dump

--- themes/kapital/templates/node/node--disco.tpl.php
+++ themes/kapital/templates/node/node--disco.tpl.php
@@ -10,20 +10,21 @@
     <p>RRSS</p>-->
   </div><!-- /.col-->
   <div class="col-sm-3">
-    
+
     <p>Botones de compra</p>
     <?php //aqui tenemos que poner varias condiciones.
-    //Si el usuario está logueado deberíamos mostrar el boton de compra del producto con el precio normal. O el producto con precio rebajado si es socixs. Y un boton de hazte socix 
+    //Si el usuario está logueado deberíamos mostrar el boton de compra del producto con el precio normal. O el producto con precio rebajado si es socixs. Y un boton de hazte socix
     //Si no está logueado, habría que poner el producto normal y un botón de hazte socixs o que si lo eres te lleve a loguearte.
     //Habrá que comprobar si el usuario es socixs. Por ejemplo, guardando la lista de socixs en una variable de drupal y comprobando el email. ?>
-    
+   <?php ?>
+    <?php var_dump($user->mail) ?>
     <?php if ($user->uid == 1): ?>
       39€
       <p><?php print disco_generar_boton_product($node) ?></p>
       <?php else: ?>
         <p><?php print disco_generar_boton_product($node, "diska-socixs") ?></p>
     <?php endif; ?>
-    
+
     <hr>
   </div><!-- /.col-->
 </div><!-- /.row -->
