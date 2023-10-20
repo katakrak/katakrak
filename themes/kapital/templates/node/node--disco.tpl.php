89928281e7e1a4ae7f200c8b34fa645d9107e11d
xare <xaresd@gmail.com>
2023-10-20 15:01:57 +0200

    CSS for diska

--- themes/kapital/templates/node/node--disco.tpl.php
+++ themes/kapital/templates/node/node--disco.tpl.php
@@ -16,20 +16,17 @@
     //Si el usuario está logueado deberíamos mostrar el boton de compra del producto con el precio normal. O el producto con precio rebajado si es socixs. Y un boton de hazte socix
     //Si no está logueado, habría que poner el producto normal y un botón de hazte socixs o que si lo eres te lleve a loguearte.
     //Habrá que comprobar si el usuario es socixs. Por ejemplo, guardando la lista de socixs en una variable de drupal y comprobando el email. ?>
-      <strong><?php echo print t('Precio Socios'); ?></strong>
+      <strong><?php echo print t('Precio Socios'); ?></strong><br />
       36 €<br />
-      <strong><?php echo print t('Precio Preventa'); ?></strong>
+      <strong><?php echo print t('Precio Preventa'); ?></strong><br />
       39 €<br />
-      <strong><?php echo print t('Precio completo, a partir del 12/12'); ?></strong>
+      <p><?php print disco_generar_boton_product($node, "diska-aurresalmenta"); ?></p>
+      <strong><?php echo print t('Precio completo, a partir del 12/12'); ?></strong><br />
       44 €<br />
       <?php if (strpos(variable_get( 'lista_socixs', '' ), $user->mail) !== false) : ?>
         <p><?php print disco_generar_boton_product($node, "diska-socixs"); ?></p>
-      <?php ?>
-
-
 
       <?php else : ?>
-        39 €
         <p><?php print disco_generar_boton_product($node); ?></p>
       <?php endif; ?>
 
