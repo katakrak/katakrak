2e087990299907152637fbfc375c4de4c57b10ee
xare <xaresd@gmail.com>
2023-10-23 21:22:11 +0200

    add new texts reorganize buttons

--- themes/kapital/templates/node/node--disco.tpl.php
+++ themes/kapital/templates/node/node--disco.tpl.php
@@ -16,21 +16,19 @@
     //Si el usuario está logueado deberíamos mostrar el boton de compra del producto con el precio normal. O el producto con precio rebajado si es socixs. Y un boton de hazte socix
     //Si no está logueado, habría que poner el producto normal y un botón de hazte socixs o que si lo eres te lleve a loguearte.
     //Habrá que comprobar si el usuario es socixs. Por ejemplo, guardando la lista de socixs en una variable de drupal y comprobando el email. ?>
-      <strong><?php print t('Precio Socios'); ?></strong><br />
+      <strong><?php print t('Socixs'); ?></strong><br />
       36 €<br />
-      <strong><?php print t('Precio Preventa'); ?></strong><br />
-      39 €<br />
-      <p><?php print disco_generar_boton_product($node, "diska-aurresalmenta"); ?></p>
-      <strong><?php print t('Precio completo, a partir del 12/12'); ?></strong><br />
-      44 €<br />
       <?php if (strpos(variable_get( 'lista_socixs', '' ), $user->mail) !== false) : ?>
         <p><?php print disco_generar_boton_product($node, "diska-socixs"); ?></p>
-
       <?php else : ?>
-        <p><?php print disco_generar_boton_product($node); ?></p>
-      <?php endif; ?>
-
-
+        <p>botón socixs</p>
+        <?php endif; ?>
+      <strong><?php print t('Preventa'); ?></strong><br />
+      39 €<br />
+      <p><?php print disco_generar_boton_product($node, "diska-aurresalmenta"); ?></p>
+      <strong><?php print t('A partir del 14 de noviembre'); ?></strong><br />
+      44 €<br />
+      <p><?php print disco_generar_boton_product($node); ?></p>
     <hr>
   </div><!-- /.col-->
 </div><!-- /.row -->
