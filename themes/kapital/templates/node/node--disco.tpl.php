15ebbdf4753f77bc89dd9f05eca6baad06535d14
xare <xaresd@gmail.com>
2023-10-23 21:51:16 +0200

    Distribute disk prices and buttons in columns

--- themes/kapital/templates/node/node--disco.tpl.php
+++ themes/kapital/templates/node/node--disco.tpl.php
@@ -3,23 +3,32 @@
     <?php print render($content['field_imagen']) ?>
   </div><!-- /.col-->
   <div class="col-sm-9">
-  <?php //aqui tenemos que poner varias condiciones.
-    //Si el usuario está logueado deberíamos mostrar el boton de compra del producto con el precio normal. O el producto con precio rebajado si es socixs. Y un boton de hazte socix
-    //Si no está logueado, habría que poner el producto normal y un botón de hazte socixs o que si lo eres te lleve a loguearte.
-    //Habrá que comprobar si el usuario es socixs. Por ejemplo, guardando la lista de socixs en una variable de drupal y comprobando el email. ?>
-      <strong><?php print t('Socixs'); ?></strong><br />
-      36 €<br />
-      <?php if (strpos(variable_get( 'lista_socixs', '' ), $user->mail) !== false) : ?>
-        <p><?php print disco_generar_boton_product($node, "diska-socixs"); ?></p>
-      <?php else : ?>
-        <p><a class="btn btn-primary" href="/eus/egin-zaitez-bazkide"><?php print t('Hazte Socix'); ?></a></p>
-        <?php endif; ?>
-      <strong><?php print t('Preventa'); ?></strong><br />
-      39 €<br />
-      <p><?php print disco_generar_boton_product($node, "diska-aurresalmenta"); ?></p>
-      <strong><?php print t('A partir del 14 de noviembre'); ?></strong><br />
-      44 €<br />
-      <p><?php print disco_generar_boton_product($node); ?></p>
+    <div class="row">
+      <?php //aqui tenemos que poner varias condiciones.
+        //Si el usuario está logueado deberíamos mostrar el boton de compra del producto con el precio normal. O el producto con precio rebajado si es socixs. Y un boton de hazte socix
+        //Si no está logueado, habría que poner el producto normal y un botón de hazte socixs o que si lo eres te lleve a loguearte.
+        //Habrá que comprobar si el usuario es socixs. Por ejemplo, guardando la lista de socixs en una variable de drupal y comprobando el email. ?>
+          <div class="col-sm-4">
+            <strong><?php print t('Socixs'); ?></strong><br />
+            36 €<br />
+            <?php if (strpos(variable_get( 'lista_socixs', '' ), $user->mail) !== false) : ?>
+              <p><?php print disco_generar_boton_product($node, "diska-socixs"); ?></p>
+            <?php else : ?>
+              <p><a class="btn btn-primary" href="/eus/egin-zaitez-bazkide"><?php print t('Hazte Socix'); ?></a></p>
+            <?php endif; ?>
+          </div>
+          <div class="col-sm-4">
+            <strong><?php print t('Preventa'); ?></strong><br />
+            39 €<br />
+          </div>
+          <div class="col-sm-4">
+            <p><?php print disco_generar_boton_product($node, "diska-aurresalmenta"); ?></p>
+          <strong><?php print t('A partir del 14 de noviembre'); ?></strong><br />
+          44 €<br />
+          <p><?php print disco_generar_boton_product($node); ?></p>
+          </div>
+
+    </div>
     <hr>
     <h1 class="mt-0"><?php print $node->title ?></h1>
     <p><?php print render($content['body']) ?></p>
