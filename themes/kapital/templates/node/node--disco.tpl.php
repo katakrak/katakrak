d0fda170234f6c5f939f74e96c0c7fe9f1599e1d
Xabier <xabier@SERVIDOR>
2023-11-15 17:46:25 +0100

    New date

--- themes/kapital/templates/node/node--disco.tpl.php
+++ themes/kapital/templates/node/node--disco.tpl.php
@@ -14,7 +14,7 @@
         //Si el usuario está logueado deberíamos mostrar el boton de compra del producto con el precio normal. O el producto con precio rebajado si es socixs. Y un boton de hazte socix
         //Si no está logueado, habría que poner el producto normal y un botón de hazte socixs o que si lo eres te lleve a loguearte.
         //Habrá que comprobar si el usuario es socixs. Por ejemplo, guardando la lista de socixs en una variable de drupal y comprobando el email. ?>
-          <?php $preventa = time() < 1699948800; //Si la fecha es inferior al 14 de noviembre ?>
+          <?php $preventa = time() < 1700611200; //Si la fecha es inferior al 22 de noviembre ?>
             <h4><strong><?php print t('Socixs'); ?></strong></h4>
             <p class="price">36 €</p>
             <?php if (strpos(variable_get( 'lista_socixs', '' ), $user->mail) !== false ) : ?>
@@ -29,7 +29,9 @@
           <p class="price">39 €</p> 
             <p><?php print disco_generar_boton_product($node, "diska-aurresalmenta"); ?></p>
             <?php endif; ?>  
-              <h4><strong><?php print t('A partir del 14 de noviembre'); ?></strong></h4>
+              <?php if ($preventa): ?>
+                <h4><strong><?php print t('A partir del 22 de noviembre'); ?></strong></h4>
+              <?php endif; ?>
               <p class="price">44 €</p>
               <p><?php print disco_generar_boton_product($node); ?></p>
             
@@ -46,7 +48,7 @@
         //Si el usuario está logueado deberíamos mostrar el boton de compra del producto con el precio normal. O el producto con precio rebajado si es socixs. Y un boton de hazte socix
         //Si no está logueado, habría que poner el producto normal y un botón de hazte socixs o que si lo eres te lleve a loguearte.
         //Habrá que comprobar si el usuario es socixs. Por ejemplo, guardando la lista de socixs en una variable de drupal y comprobando el email. ?>
-          <?php $preventa = time() < 1699948800; //Si la fecha es inferior al 14 de noviembre ?>
+          <?php $preventa = time() < 1700611200; //Si la fecha es inferior al 22 de noviembre ?>
             <h4><strong><?php print t('Socixs'); ?></strong></h4>
             <p class="price">36 €</p>
             <?php if (strpos(variable_get( 'lista_socixs', '' ), $user->mail) !== false ) : ?>
@@ -61,7 +63,9 @@
           <p class="price">39 €</p> 
             <p><?php print disco_generar_boton_product($node, "diska-aurresalmenta"); ?></p>
             <?php endif; ?>  
-              <h4><strong><?php print t('A partir del 14 de noviembre'); ?></strong></h4>
+            <?php if ($preventa): ?>
+                <h4><strong><?php print t('A partir del 22 de noviembre'); ?></strong></h4>
+              <?php endif; ?>
               <p class="price">44 €</p>
               <p><?php print disco_generar_boton_product($node); ?></p>
             
