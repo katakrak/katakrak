b01b4ad6bb55a0f8ce02738b6dd17a9534b62bae
Xabier <xabier@SERVIDOR>
2024-01-09 14:22:51 +0100

    Numero seguimiento

--- themes/kapital/templates/system/commerce_order__commerce_order.tpl.php
+++ themes/kapital/templates/system/commerce_order__commerce_order.tpl.php
@@ -111,7 +111,9 @@ $shipping = $shipping->commerce_customer_address['und'][0];
                       <th><?php print $forma_envio ?></th>
                       <?php if (katakrak_commerce_cart_is_mensajeria($commerce_order)): ?>
                       <td class="text-right">
-                        <a class="btn btn-primary" href="https://s.correosexpress.com/SeguimientoSinCP/search?request_locale=es_ES&shippingNumber=<?php print $commerce_order->order_number?>" target="_blank"><?php print t('Seguimiento del pedido') ?></a>
+                        <?php if ($commerce_order->field_numero_seguimiento['und'][0]['value']): ?>
+                        <a class="btn btn-primary" href="https://s.correosexpress.com/SeguimientoSinCP/search?request_locale=es_ES&shippingNumber=<?php print $commerce_order->field_numero_seguimiento['und'][0]['value']?>" target="_blank"><?php print t('Seguimiento del pedido') ?></a>
+                        <?php endif; ?>
                       </td>
                       <?php endif; ?>
                     </tr>
